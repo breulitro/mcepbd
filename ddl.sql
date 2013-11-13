c55a337f5c67b3eedd153cbd93a25ac2157c8a9b
Benito Michelon <benito@localhost.localdomain>
2013-11-13 14:40:45 -0200

    Modificado PedidoLoja e ItemPedido Concluída a inserção de dados
    Signed-off-by: Benito Michelon <benito@localhost.localdomain>

--- ddl.sql
+++ ddl.sql
@@ -11,18 +11,6 @@ drop table if exists Produto;
 drop table if exists Setor;
 drop table if exists Funcionarios;
 
----------------------------------------------------------------------------------
---Oracle
---http://lmgtfy.com/?q=CREATE SEQUENCE
---Access
---nro_matricula INTEGER AUTOINCREMENT PRIMARY KEY,
---SQL Server
---nro_matricula INTEGER(4) IDENTITY(1,1) PRIMARY KEY,
---SQLite
---nro_matricula INTEGER(4) PRIMARY KEY NOT NULL AUTOINCREMENT,
---MySQL
--- eh o que ta sendo usado, mas eh tao tosco que nao aceita comentario no meio
---do create table... t~ao tosco quanto as configs do meu teclado :¬)
 CREATE TABLE Funcionarios (
 nro_matricula INTEGER(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome VARCHAR(80) NOT NULL,
@@ -30,15 +18,17 @@ data_admissao Date NOT NULL,
 email VARCHAR(80),
 ramal INTEGER(4),
 salario NUMERIC(15,2) NOT NULL,
-cpf VARCHAR(12) NOT NULL
+cpf VARCHAR(12) NOT NULL UNIQUE
 );
 
 insert into Funcionarios (nome, data_admissao, email, ramal, salario, cpf)
 values	("Percival", "1956-04-01", "perci@val.com", 5467, 634.33, "132414234-33"),
 		("Roberval", "1979-02-23", "rober@val.com", 3422, 379.27, "987234756-13"),
-		("Pafuncio", "1978-04-28", "pafun@cio.com", 3123, 279.27, "987234756-12");
+		("Pafuncio", "1978-04-28", "pafun@cio.com", 3123, 279.27, "987234756-12"),
+		("Bartholomeo", "1928-11-23", "bar@tho.lomeo", 2312, 1980.00, "123443223-12"),
+		("Geraldison", "1976-02-29", "ger@ldis.on", 1231, 459.29, "623434534-34"),
+		("Geraldaughter", "1976-02-29", "gerald@ugh.ter", 1232, 459.28, "623434534-35");
 
----------------------------------------------------------------------------------
 CREATE TABLE Setor (
 cod_setor INTEGER(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 email VARCHAR(80),
@@ -52,7 +42,6 @@ values	("in@fan.~", 1234, "Infantil", 3421),
 		("moveis@loja.com", 3453, "Moveis", 353),
 		("cama@mesa.banho", 5432, "Cama Mesa e Banho", 6543);
 
----------------------------------------------------------------------------------
 CREATE TABLE Produto (
 cod_produto INTEGER(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 descricao VARCHAR(100) NOT NULL,
@@ -65,7 +54,6 @@ values	("Calcinha", 11.32),
 		("Roupeiro", 543.23),
 		("Travesseiro", 12.99);
 
----------------------------------------------------------------------------------
 CREATE TABLE GerentesSetor (
 cod_gerente INTEGER(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nro_matricula INTEGER(4) NOT NULL,
@@ -74,13 +62,11 @@ celular INTEGER(14),
 FOREIGN KEY(nro_matricula) REFERENCES Funcionarios (nro_matricula),
 FOREIGN KEY(cod_setor) REFERENCES Setor (cod_setor)
 );
---ALTER TABLE GerentesSetor AUTO_INCREMENT = 1000;
 insert into GerentesSetor (nro_matricula, cod_setor, celular)
 values	(3, 3, 67474356),
 		(2, 2, 98712322),
 		(1, 1, 94572289);
 
----------------------------------------------------------------------------------
 CREATE TABLE Lojas (
 cod_filial INTEGER(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 endereco VARCHAR(100) NOT NULL,
@@ -93,7 +79,6 @@ values	("Avenida Passo Manco 171", 32552345, "filial@manca.br"),
 		("Travessa Paralela 1313", 32423243, "filial@travessa.br"),
 		("Rua Paranguape 1234", 32255896, "filial@paranguape.br");
 
----------------------------------------------------------------------------------
 CREATE TABLE ProdutosLoja (
 cod_filial INTEGER(4) NOT NULL,
 cod_produto INTEGER(4) NOT NULL,
@@ -113,7 +98,6 @@ values  (3, 1, 20, 200, 140),
 		(1, 2, 2, 10, 6),
 		(1, 1, 10, 100, 22);
 
----------------------------------------------------------------------------------
 CREATE TABLE SetorProduto (
 cod_setor INTEGER(4) NOT NULL,
 cod_produto INTEGER(4) NOT NULL,
@@ -129,18 +113,6 @@ values	(3, 2, 32),
 		(2, 3, 30),
 		(1, 1, 500);
 
----------------------------------------------------------------------------------
-CREATE TABLE ItemPedido (
-cod_item_pedido INTEGER(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-cod_produto INTEGER(4) NOT NULL,
-quantidade INTEGER(4) NOT NULL,
-FOREIGN KEY(cod_produto) REFERENCES Produto (cod_produto)
-);
-
-insert into ItemPedido (cod_produto, quantidade)
-values (1, 20);
-
----------------------------------------------------------------------------------
 CREATE TABLE GerentesLoja (
 cod_gerente INTEGER(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nro_matricula INTEGER(4) NOT NULL,
@@ -151,26 +123,42 @@ FOREIGN KEY(cod_filial) REFERENCES Lojas (cod_filial)
 );
 
 insert into GerentesLoja (nro_matricula, cod_filial, meta_mensal)
-values (1, 1, 1000.00);
+values	(4, 1, 1000.00),
+		(5, 2, 2000.00),
+		(6, 3, 3000.00);
 
----------------------------------------------------------------------------------
+
 CREATE TABLE PedidoLoja (
 cod_pedido INTEGER(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 cod_gerente INTEGER(4) NOT NULL,
-cod_item_pedido INTEGER(4) NOT NULL,
 urgente char(1) DEFAULT 'n',
 data Date NOT NULL,
-FOREIGN KEY(cod_gerente) REFERENCES GerentesLoja (cod_gerente),
-FOREIGN KEY(cod_item_pedido) REFERENCES ItemPedido (cod_item_pedido)
+FOREIGN KEY(cod_gerente) REFERENCES GerentesLoja (cod_gerente)
 );
 
-insert into PedidoLoja (cod_gerente, cod_item_pedido, urgente, data)
-values (1, 1, 'y', "2012-03-31");
--- Se omitir o campo 'urgente' ele assume o default ;)
-insert into PedidoLoja (cod_gerente, cod_item_pedido, data)
-values (1, 1, "2012-03-31");
+insert into PedidoLoja (cod_gerente, urgente, data)
+values (1, 'y', "2012-03-31");
+insert into PedidoLoja (cod_gerente, data)
+values (2, "2012-03-31");
 
----------------------------------------------------------------------------------
+
+CREATE TABLE ItemPedido (
+cod_pedido INTEGER(4) NOT NULL,
+cod_produto INTEGER(4) NOT NULL,
+quantidade INTEGER(4) NOT NULL,
+PRIMARY KEY(cod_pedido, cod_produto),
+FOREIGN KEY(cod_produto) REFERENCES Produto (cod_produto),
+FOREIGN KEY(cod_pedido) REFERENCES PedidoLoja (cod_pedido)
+);
+
+insert into ItemPedido (cod_pedido, cod_produto, quantidade)
+values	(1, 1, 20),
+		(1, 2, 3),
+		(1, 3, 6),
+		(2, 1, 12),
+		(2, 2, 4),
+		(2, 4, 9);
+
 CREATE TABLE Aprovacoes (
 cod_gerente INTEGER(4) NOT NULL,
 cod_pedido INTEGER(4) NOT NULL,
@@ -182,5 +170,3 @@ FOREIGN KEY(cod_pedido) REFERENCES PedidoLoja (cod_pedido)
 
 insert into Aprovacoes (cod_gerente, cod_pedido, data)
 values (1, 1, "2012-04-01");
---se quiser alterar o valor inicial na tabela GerentesSetor
---values (1000, 1, "2012-04-01");
